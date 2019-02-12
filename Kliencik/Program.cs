180c0493fb1199345566b9f9d57ee862781294a4
mgremek <k.pawlak666@gmail.com>
2019-02-12 18:37:14 +0100

    first commit

---
+++ Kliencik/Program.cs
@@ -0,0 +1,26 @@
+﻿using ProduktyService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kliencik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost h = new ServiceHost(typeof(ProduktyService2.ProduktyService));
+            h.Opened += H_Opened;
+            h.Open();
+            Console.Read(); 
+        }
+
+        private static void H_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("Działa");
+        }
+    }
+}
---
+++ ProduktyService2/Service1.cs
@@ -0,0 +1,38 @@
+﻿using ProduktyService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProduktyService2
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę klasy „Service1” w kodzie i pliku konfiguracji.
+    public class ProduktyService : IProdukty
+    {
+        WarKonfiguratorEntities wke;
+        public ProduktyService()
+        {
+            wke = new WarKonfiguratorEntities();
+        }
+
+        public List<Manufacturers> GetManufacturers()
+        {
+            return wke.Manufacturers.ToList();
+        }
+
+        public List<Products_TEST> GetProdukty()
+        {
+            //return wke.Products_TEST.ToList();
+            return wke.Products_TEST.Select(p=>new {ManID=p.ManID,Model=p.Model,Price=p.Price,SubId=p.SubID, Specification="[niedostępne w tym widoku]"}).Take(200).ToList()
+                .ConvertAll<Products_TEST>(c=>new Products_TEST {  ManID = c.ManID, Model = c.Model, Price = c.Price, SubID=c.SubId, Specification=c.Specification });
+        }
+
+        public List<SubCategory> GetSubcategory()
+        {
+            return wke.SubCategory.ToList();
+        }
+
+    }
+}
